35785dac7f1b83009847beb94ee235da87269788
a87920854 <a87920854@gmail.com>
2022-06-13 16:19:02 +0800

    jack_v1.0

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,35 @@
+import { useIntl } from 'umi';
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-layout';
+
+const Footer: React.FC = () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    defaultMessage: '微電能源股份有限公司 RFD Micro Electricity Co., Ltd. All rights reserved.',
+  });
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        // {
+        //   key: 'Ant Design Pro',
+        //   title: 'Ant Design Pro',
+        //   href: 'https://pro.ant.design',
+        //   blankTarget: true,
+        // },
+        // {
+        //   key: 'Ant Design',
+        //   title: 'Ant Design',
+        //   href: 'https://ant.design',
+        //   blankTarget: true,
+        // },
+      ]}
+    />
+  );
+};
+
+export default Footer;
